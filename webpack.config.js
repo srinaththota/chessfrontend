1e369c21e323b067c704b78cfacced022f5eda03
Srinath Thota <ext-srinath.thota@here.com>
2020-01-01 23:48:13 +0530

    include css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,35 @@ module.exports={
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                loader:'babel-loader'
+            },
+            {
+                test:/\.css$/,
+                exclude:/node_modules/,
+                use:[
+                        {loader:'style-loader'},
+                        {loader:'css-loader',ptions:{
+                            importLoaders:1,
+                            modules:{
+                                localIdentName:'[name]__[local]__[hash:base64:5]'
+                            }
+                        },
+                    },
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            ident:'postcss',
+                            plugins:()=>[autoprefixer()]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
