e9cd02598a8aeb8d3746c63d3fc78b5c32e6c1c3
Srinath Thota <ext-srinath.thota@here.com>
2020-01-01 22:20:55 +0530

    added index.js and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path=require('path');
 module.exports={
 
+    entry:{
+        app:path.resolve(__dirname,'src/index.js')
+    },
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'bundle.js'
+    }
 }
